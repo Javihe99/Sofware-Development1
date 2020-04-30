6ecfe19efa152641ad8595e1ec9aec9ba53e976d
hdkhk <100386157@alumnos.uc3m.es>
2020-04-30 23:13:22 +0200

    crear clase y paquete para el hash

--- Cod_INI_EG4_2/src/main/java/Transport4Future/TokenManagement/TokenManager.java
+++ Cod_INI_EG4_2/src/main/java/Transport4Future/TokenManagement/TokenManager.java
@@ -30,6 +30,7 @@ import Transport4Future.TokenManagement.Exception.TokenManagementException;
 import Transport4Future.TokenManagement.Parser.JSONFileParser;
 import Transport4Future.TokenManagement.Store.TokenRequestStore;
 import Transport4Future.TokenManagement.Store.TokensStore;
+import Transport4Future.TokenManagement.Utils.Hash;
 
 import java.lang.reflect.Type;
 
@@ -41,45 +42,12 @@ public class TokenManager implements ITokenManagement {
 		JSONFileParser myFile = new JSONFileParser();
 		JsonObject jsonLicense = myFile.parseJSONFile(InputFile);	
 		TokenRequest req = createTokenRequest(jsonLicense);
-		String hex = generateHashMD5(req);
+		Hash myHash = new Hash();
+		String hex = myHash.generateHashMD5(req);
 		TokenRequestStore myStore = new TokenRequestStore();
         myStore.storeTokenRequest(req, hex);
 		//Devolver el hash
 		return hex;
-	}
-
-	private String generateHashMD5(TokenRequest req) throws TokenManagementException {
-		MessageDigest md;
-		try {
-			md = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e) {
-			throw new TokenManagementException("Error: no such hashing algorithm.");
-		}
-		
-		String input =  "Stardust" + "-" + req.toString();
-		
-		md.update(input.getBytes(StandardCharsets.UTF_8));
-		byte[] digest = md.digest();
-
-		// Beware the hex length. If MD5 -> 32:"%032x", but for instance, in SHA-256 it should be "%064x" 
-		String hex = String.format("%32x", new BigInteger(1, digest));
-		return hex;
-	}
-
-	private String generateHashSHA256(String dataToSign) throws TokenManagementException {
-		MessageDigest md;
-		try {
-			md = MessageDigest.getInstance("SHA-256");
-		} catch (NoSuchAlgorithmException e) {
-			throw new TokenManagementException("Error: no such hashing algorithm.");
-		}
-
-		md.update(dataToSign.getBytes(StandardCharsets.UTF_8));
-		byte[] digest = md.digest();
-
-		// Beware the hex length. If MD5 -> 32:"%032x", but for instance, in SHA-256 it should be "%064x"
-		String signature = String.format("%064x", new BigInteger(1, digest));
-		return signature;
 	}
 
 	
@@ -118,7 +86,8 @@ public class TokenManager implements ITokenManagement {
 
 		
 		String dataToSign =myToken.getHeader() + myToken.getPayload();
-		String signature = generateHashSHA256(dataToSign);
+		Hash myHash = new Hash();
+		String signature = myHash.generateHashSHA256(dataToSign);
 
 		myToken.setSignature(signature);
 		
---
+++ Cod_INI_EG4_2/src/main/java/Transport4Future/TokenManagement/Utils/Hash.java
@@ -0,0 +1,47 @@
+package Transport4Future.TokenManagement.Utils;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import Transport4Future.TokenManagement.Data.TokenRequest;
+import Transport4Future.TokenManagement.Exception.TokenManagementException;
+
+public class Hash {
+	public String generateHashMD5(TokenRequest req) throws TokenManagementException {
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new TokenManagementException("Error: no such hashing algorithm.");
+		}
+		
+		String input =  "Stardust" + "-" + req.toString();
+		
+		md.update(input.getBytes(StandardCharsets.UTF_8));
+		byte[] digest = md.digest();
+
+		// Beware the hex length. If MD5 -> 32:"%032x", but for instance, in SHA-256 it should be "%064x" 
+		String hex = String.format("%32x", new BigInteger(1, digest));
+		return hex;
+	}
+
+	public String generateHashSHA256(String dataToSign) throws TokenManagementException {
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			throw new TokenManagementException("Error: no such hashing algorithm.");
+		}
+
+		md.update(dataToSign.getBytes(StandardCharsets.UTF_8));
+		byte[] digest = md.digest();
+
+		// Beware the hex length. If MD5 -> 32:"%032x", but for instance, in SHA-256 it should be "%064x"
+		String signature = String.format("%064x", new BigInteger(1, digest));
+		return signature;
+	}
+
+
+}
